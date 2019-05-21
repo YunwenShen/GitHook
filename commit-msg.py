2f8304b9d43f56e4cf9a4e063b9e9b209e1a8ba4
shenyw <shenyw@citycloud.com.cn>
2019-05-21 10:18:26 +0800

    feat(*): git commit-msg hook

---
+++ commit-msg.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import sys
+import re
+
+pattern = re.compile(
+    "^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types)(\(.+\))?: .{1,50}")
+
+
+def validate_commit_msg(msg: str):
+    """
+    校验git commit 内容格式是否满足要求
+    :param msg:
+    :return: void
+    """
+    if re.match(pattern, msg):
+        sys.exit(0)
+    else:
+        print("invalid commit format")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    print(file_name)
+    commit_msg = ""
+    with open(file_name, encoding="utf-8") as file:
+        for line in file.readlines():
+            commit_msg += line
+    validate_commit_msg(commit_msg)
